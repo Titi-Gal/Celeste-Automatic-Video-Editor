3f86f83f6cd0b501b154acf41102654f29c04e89
ThiagoLRG <54580798+ThiagoLRG@users.noreply.github.com>
2022-07-10 14:49:50 -0300

    added debug flag to aid ajust crop and blackdetect

--- celeste_automatic_video_editor.py
+++ celeste_automatic_video_editor.py
@@ -12,10 +12,11 @@ you can opt to have the last try/section in real time and/or complete.""", epilo
     parser.add_argument('--long_scroll_screen', type=bool, default=False, help="intended for long scroll screen like last fro 8C or last from Moon, it will edit tries from short to long", metavar="")
     parser.add_argument('--secs_before_cut', type=float, default=5, help="hou much video to keep before each cut, this is before speed change", metavar="")
     parser.add_argument('--speed_change', type=float, default=2, help="by how much to change the speed. 1 is no change, can't be 0 and to hight will affect video/audio quality", metavar="")
+    parser.add_argument('--debug_crop_blackdetect', type=bool, default=False, help="saves the cropped video blackdetect is receiving and saves a .txt with all video sections to help ajust crop and blackdetect", metavar="")
     parser.add_argument('--crop_size', type=int, nargs=2, default=[300,300], help="rectangular area to cut in pixels", metavar="")
-    parser.add_argument('--crop_center', type=int, nargs=2, default=['(iw/2)','(ih/2)'], help="center of the rectangular area", metavar="")
-    parser.add_argument('--blackdetect_duration', type=float, default=0.15, help="min duration for a screen to be considered black", metavar="")
-    parser.add_argument('--blackdetect_pix_th', type=float, default=0.02, help="pixel threshold: max luminescence for a pixel to be considered black", metavar="")
+    parser.add_argument('--crop_center', type=int, nargs=2, default=['(iw/2)','(ih/2)'], help="center of the rectangular area, if your game if not in fullscreen you will problably have to ajust this to be he center of your game", metavar="")
+    parser.add_argument('--blackdetect_duration', type=float, default=0.1, help="min duration for a screen to be considered black", metavar="")
+    parser.add_argument('--blackdetect_pix_th', type=float, default=0.03, help="pixel threshold: max luminescence for a pixel to be considered black", metavar="")
     parser.add_argument('--blackdetect_pic_th', type=float, default=1, help="picture threshold: percentage of the screen that has to be <= pix_th to be considered black", metavar="")
     args = parser.parse_args()
     
@@ -27,6 +28,7 @@ you can opt to have the last try/section in real time and/or complete.""", epilo
     last_try_normal_speed = args.last_try_normal_speed
     long_scroll_screen = args.long_scroll_screen
     # Crop variables
+    debug_crop_blackdetect = args.debug_crop_blackdetect
     w = args.crop_size[0]
     h = args.crop_size[1]
     x = args.crop_center[0]
@@ -56,14 +58,18 @@ you can opt to have the last try/section in real time and/or complete.""", epilo
         main_video_format = main_video[main_video.rindex(".") + 1:]
         main_video_name = main_video[main_video.rindex(f"\\") + 1: main_video.rindex(".")]
         ready_video = f'{main_dir}\\{main_video_name}_edited.{main_video_format}'
-        temp_dir = f'{main_dir}\\TEMP_FILES_CELESTE_DEATHS_AUTOMAIC_VIDEO_EDITOR'
+        temp_dir = f'{main_dir}\\celeste_automatic_video_editor_temp_can_be_delete_if_its_not_running'
         os.mkdir(temp_dir)
+        cropped_video = f'-f null -'
+        if debug_crop_blackdetect: # Saves cropped video
+            cropped_video = f'{main_dir}\\{main_video_name}_cropped.{main_video_format}'
+
         #____________________________________________________________________________________
         # Build a list of sections of the video to cut by identifying blackscreen transitions
         print("detecting blackscreen transitions, this can take a while")
 
         # From ffmpeg stderrout only lines that contains "blackdetect"
-        ffmpeg_command = f'ffmpeg -i "{main_video}" -vf crop={w}:{h}:{x}-{w/2}:{y}-{h/2},blackdetect=d={duration}:pix_th={pix_th}:pic_th={pic_th} -f null -'
+        ffmpeg_command = f'ffmpeg -i "{main_video}" -vf crop={w}:{h}:{x}-{w/2}:{y}-{h/2},blackdetect=d={duration}:pix_th={pix_th}:pic_th={pic_th} {cropped_video}'
         ffmpeg_capture_out = [line for line in ffmpeg_captureOut(ffmpeg_command) if "blackdetect" in line]
 
         # Format bkackdetect lines into dict
@@ -79,18 +85,26 @@ you can opt to have the last try/section in real time and/or complete.""", epilo
         # Build a list with all places to cut, which are in the middle of blacks transitions
         places_to_cut = [blackdetect["black_start"] + ((blackdetect["black_end"] - blackdetect["black_start"]) / 2) for blackdetect in ffmpeg_blackdetect]
         # Build a list with sections of video to be cutted, with start and end places for each section
-        sections_to_cut = [{"section_start": places_to_cut[i], "section_end": places_to_cut[i + 1]} for i in range(len(places_to_cut) - 1)]
-        # If long scroll screen list is sorted to short video sections first
+        sections_to_cut = [{"section_start": places_to_cut[i] , "section_end": places_to_cut[i + 1]} for i in range(len(places_to_cut) - 1)]
+
+        # If debug flag saves a txt with videos video sections
+        if debug_crop_blackdetect:
+            with open(f'{main_dir}\\debug_video_sections.txt', 'a') as file:
+                file.write(f'{main_video_name}.{main_video_format}\n')
+                for i, section in enumerate(sections_to_cut):
+                    file.write(f"{i + 1:0>3}. section_start:{secs_to_timecode_str(section['section_start'])} section_end: {secs_to_timecode_str(section['section_end'])}\n")
+
+        # If long scroll screen flag list is sorted to short video sections first
         if long_scroll_screen:
             sections_to_cut.sort(key=lambda x: x["section_end"] - x["section_start"])
         del places_to_cut
 
-        # If no transition was detected quit
+        # If no transition was detected
         if sections_to_cut == []:
-            print(f"no transitions detected in {main_video_name}.{main_video_format}, check your video or try ajusting crop and blackdetect")
+            print(f"no transitions detected in {main_video_name}.{main_video_format}, ajust crop and blackdetect, try --debug_crop_blackdetect True")
 
         #_____________________________________________
-        # Create video section files eith speed change
+        # Create video section files with speed change
         print(f"video sections detected: {len(sections_to_cut)}")
 
         for i, section in enumerate(sections_to_cut):
@@ -102,16 +116,16 @@ you can opt to have the last try/section in real time and/or complete.""", epilo
             elif start < section["section_start"]:
                 start = section["section_start"]
             end = section["section_end"]
-            
+            ffmpeg_command = f'ffmpeg -v 24 -stats  -ss {start} -to {end} -i "{main_video}" -vf "setpts={1/speed_change}*PTS" -filter:a "atempo={1*speed_change}" "{temp_dir}\\section{i:0>6}.{main_video_format}"'  
+
             # Last section options: complete try/normal speed
             if i == len(sections_to_cut) - 1:
                 if last_try_complete: # Starts from the beguining of section
                     start = section["section_start"]
                 if last_try_normal_speed: # Change ffmpeg command to normal speed
-                    speed_change = 1
+                    ffmpeg_command = f'ffmpeg -v 24 -stats  -ss {start} -to {end} -i "{main_video}" "{temp_dir}\\section{i:0>6}.{main_video_format}"' 
             
-            # Create ffmpeg command and execute it
-            ffmpeg_command = f'ffmpeg -v 24 -stats  -ss {start} -to {end} -i "{main_video}" -vf "setpts={1/speed_change}*PTS" -filter:a "atempo={1*speed_change}" "{temp_dir}\\section{i:0>6}.{main_video_format}"'   
+            # Create ffmpeg command and execute it 
             ffmpeg_execute(ffmpeg_command)
         
         #___________________________
@@ -166,6 +180,20 @@ def only_supported_file_format_paths(videos_directory_path, ffmpeg_supprted_form
             if file_format in ffmpeg_supprted_formats:
                 supported_videos_file_paths.append(f"{videos_directory_path}\\{file_name}")
     return sorted(supported_videos_file_paths)
+
+def secs_to_timecode_str(seconds):
+    times = 0
+    timecode_str = ''
+    while True:
+        while seconds / (60 ** times) > 60:
+            times += 1
+        if times == 0:
+            break
+        timecode_str += f'{int(seconds / (60 ** times)):0>2}:'
+        seconds -= (60 ** times) * int(seconds / (60 ** times))
+        times = 0
+    timecode_str += f'{int(seconds):0>2}:{int(round((seconds % 1), 4) * 10000):0>4}'
+    return timecode_str
 
 if __name__ == "__main__":
     main()
